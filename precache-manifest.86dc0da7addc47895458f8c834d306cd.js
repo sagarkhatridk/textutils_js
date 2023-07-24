51cd97e838ec8c2c0f0c7fcff9208d08c33fcc21
sagarkhatridk <sagarkhatridk@gmail.com>
2023-07-24 11:39:19 +0530

    Updates

---
+++ precache-manifest.86dc0da7addc47895458f8c834d306cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f96358dfa15b0bf5a2ac",
+    "url": "/textutils_js/static/css/main.ce83e7df.chunk.css"
+  },
+  {
+    "revision": "f96358dfa15b0bf5a2ac",
+    "url": "/textutils_js/static/js/main.f96358df.chunk.js"
+  },
+  {
+    "revision": "3a79397ac1a1b3a66e79",
+    "url": "/textutils_js/static/js/1.3a79397a.chunk.js"
+  },
+  {
+    "revision": "9183bb971fe7e622e880",
+    "url": "/textutils_js/static/js/2.9183bb97.chunk.js"
+  },
+  {
+    "revision": "b1d9a60ed461c6bedfa0",
+    "url": "/textutils_js/static/js/runtime~main.b1d9a60e.js"
+  },
+  {
+    "revision": "b85a4c8d232c180d43f1b7ac51b01036",
+    "url": "/textutils_js/index.html"
+  }
+];
